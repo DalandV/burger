27795ea689200454d9940cf6651c96f5afa9553b
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2019-02-23 14:38:10 -0500

    Removed devoured from post route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,13 +14,10 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.insertOne(
-    ["burger_name", "devoured"],
-    [req.body.burger_name, req.body.devoured],
-    function(result) {
-      res.json({ id: result.insertId });
-    }
-  );
+  burger.insertOne(["burger_name"], [req.body.burger_name], function(result) {
+    console.log(req.body);
+    res.json({ id: result.insertId });
+  });
 });
 
 router.put("/api/burgers/:id", function(req, res) {
